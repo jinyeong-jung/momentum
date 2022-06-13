c0a6aaef5ef3f73d437a36ef1df6b15445633c07
Jinyeong Jung <jinyeong32@gmail.com>
2022-06-13 20:16:54 +0900

    todo delete

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const list = event.target.parentElement;
+    list.remove();
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
-    const span = document.createElement("span")
-    li.appendChild(span);
+
+    const span = document.createElement("span");
     span.innerText = newTodo;
+
+    const button = document.createElement("button");
+    button.innerText = "✔️";
+
+    button.addEventListener("click", deleteToDo);
+
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
